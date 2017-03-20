180a88968d9c157b64ce0a1c351dd4ca32db2d6a
willpile <wpile@Macbook-8b76a6-2.local>
2017-03-20 16:42:35 -0500

    Added programming for TV’s in the Gathering space.  Let’s see if this works.

---
+++ Automation/tv_auto-off.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# location:
+# /opt/scripts/projector-automation/tv_auto-off.sh
+
+
+# -----
+# Gathering Space
+# -----
+
+# GS Cabinet HL / 
+wget -o /dev/null -O /dev/null "http://10.40.30.85/cgi-bin/cgi.pl?cmd=ka%200%2000%0D"
+# GS Cabinet HR / 
+wget -o /dev/null -O /dev/null "http://10.40.30.86/cgi-bin/cgi.pl?cmd=ka%200%2000%0D"
+# GS Portrait 1 / 
+wget -o /dev/null -O /dev/null "http://10.40.30.87/cgi-bin/cgi.pl?cmd=ka%200%2000%0D"
+# GS Portrait 2 / 
+wget -o /dev/null -O /dev/null "http://10.40.30.88/cgi-bin/cgi.pl?cmd=ka%200%2000%0D"
+# GS Portrait 3 / 
+wget -o /dev/null -O /dev/null "http://10.40.30.89/cgi-bin/cgi.pl?cmd=ka%200%2000%0D"
+# GS Portrait 4 / 
+wget -o /dev/null -O /dev/null "http://10.40.30.90/cgi-bin/cgi.pl?cmd=ka%200%2000%0D"
+# GS Portrait 5 / 
+wget -o /dev/null -O /dev/null "http://10.40.30.91/cgi-bin/cgi.pl?cmd=ka%200%2000%0D"
+# GS Portrait 6 / 
+wget -o /dev/null -O /dev/null "http://10.40.30.92/cgi-bin/cgi.pl?cmd=ka%200%2000%0D"
+
+
+
+
+# ***** ***** *****
+# ***** ***** *****
+
+#EOF
