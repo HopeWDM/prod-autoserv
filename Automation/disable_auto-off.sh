740620570c96442a4a81bc720a5fbde82cd0c199
cf8geek <cf8geek@users.noreply.github.com>
2017-03-28 17:15:39 -0500

    Update disable_auto-off.sh
    changed "projector" to "display".

--- Automation/disable_auto-off.sh
+++ Automation/disable_auto-off.sh
@@ -7,7 +7,7 @@ echo -e "\n\n"
 
 cp /var/www/html/javascript/auto-off_disabled.txt /var/www/html/javascript/auto-off.js
 
-echo -e "\n\n" >> /opt/log/projector-automation.log
-date +%F_%H%M%S >> /opt/log/projector-automation.log
-echo "Disabled auto-off." >> /opt/log/projector-automation.log
+echo -e "\n\n" >> /opt/log/display-automation.log
+date +%F_%H%M%S >> /opt/log/display-automation.log
+echo "Disabled auto-off." >> /opt/log/display-automation.log
 #EOF
