0dab64d825eed8bd2a9531cd4af48392ce6c56dd
cf8geek <cf8geek@users.noreply.github.com>
2017-03-28 16:37:57 -0500

    Update archive-log.sh
    changed "projector" to "display".

--- Automation/archive-log.sh
+++ Automation/archive-log.sh
@@ -5,9 +5,9 @@
 echo 'Content-type: text/html'
 echo -e "\n\n"
 
-cp /var/www/html/log/projector-automation.log /opt/log/archive/projector-automation_`date +%F_%H%M%S`.log
-echo '' > /var/www/html/log/projector-automation.log
-echo -e "\n\n" >> /opt/log/projector-automation.log
-date +%F_%H%M%S >> /opt/log/projector-automation.log
-echo "Archived previous log entries." >> /opt/log/projector-automation.log
+cp /var/www/html/log/display-automation.log /opt/log/archive/display-automation_`date +%F_%H%M%S`.log
+echo '' > /var/www/html/log/display-automation.log
+echo -e "\n\n" >> /opt/log/display-automation.log
+date +%F_%H%M%S >> /opt/log/display-automation.log
+echo "Archived previous log entries." >> /opt/log/display-automation.log
 #EOF
