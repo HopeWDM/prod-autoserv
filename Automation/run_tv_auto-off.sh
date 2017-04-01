c782c0337ffca0e121fd438b4fe1193622c453fb
cf8geek <cf8geek@users.noreply.github.com>
2017-04-01 14:10:41 -0500

    Update run_tv_auto-off.sh
    changed projector- to display-

--- Automation/run_tv_auto-off.sh
+++ Automation/run_tv_auto-off.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # location:
-# /opt/scripts/projector-automation/run_tv_auto-off.sh
+# /opt/scripts/display-automation/run_tv_auto-off.sh
 
-/opt/scripts/projector-automation/tv_auto-off.sh
+/opt/scripts/display-automation/tv_auto-off.sh
 
-echo -e "\n\n" >> /opt/log/projector-automation.log
-date +%F_%H%M%S >> /opt/log/projector-automation.log
-echo "Ran tv auto-off script." >> /opt/log/projector-automation.log
+echo -e "\n\n" >> /opt/log/display-automation.log
+date +%F_%H%M%S >> /opt/log/display-automation.log
+echo "Ran tv auto-off script." >> /opt/log/display-automation.log
 
 #EOF
