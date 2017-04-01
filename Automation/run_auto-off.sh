01d4d94da8e8f469b8753ac5e2c74d9b0cc883c6
cf8geek <cf8geek@users.noreply.github.com>
2017-04-01 14:05:27 -0500

    Update run_auto-off.sh
    moved to display- instead of projector-

--- Automation/run_auto-off.sh
+++ Automation/run_auto-off.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # location:
-# /opt/scripts/projector-automation/run_auto-off.sh
+# /opt/scripts/display-automation/run_auto-off.sh
 
-/opt/scripts/projector-automation/auto-off.sh
-/opt/scripts/projector-automation/tv_auto-off.sh
+/opt/scripts/display-automation/auto-off.sh
+/opt/scripts/display-automation/tv_auto-off.sh
 
-echo -e "\n\n" >> /opt/log/projector-automation.log
-date +%F_%H%M%S >> /opt/log/projector-automation.log
-echo "Ran auto-off script." >> /opt/log/projector-automation.log
+echo -e "\n\n" >> /opt/log/display-automation.log
+date +%F_%H%M%S >> /opt/log/display-automation.log
+echo "Ran auto-off script." >> /opt/log/display-automation.log
 
 #EOF
