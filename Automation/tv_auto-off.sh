d937a32b1346565b96f82e5f5089ae1b8605fa05
willpile <willpile@users.noreply.github.com>
2018-01-30 09:42:23 -0600

    Added Jordan Creek Prayer TV and Pillar TVs

--- Automation/tv_auto-off.sh
+++ Automation/tv_auto-off.sh
@@ -1,31 +1,37 @@
-#!/bin/bash
+!/bin/bash
 
 # location:
-# /opt/scripts/projector-automation/tv_auto-off.sh
+# /opt/scripts/display-automation/tv_auto-off.sh
 
 
 # -----
 # Gathering Space
 # -----
 
-# GS Cabinet HL / 
-wget -o /dev/null -O /dev/null "http://10.40.30.85/cgi-bin/cgi.pl?cmd=ka%200%2000%0D"
-# GS Cabinet HR / 
-wget -o /dev/null -O /dev/null "http://10.40.30.86/cgi-bin/cgi.pl?cmd=ka%200%2000%0D"
-# GS Portrait 1 / 
-wget -o /dev/null -O /dev/null "http://10.40.30.87/cgi-bin/cgi.pl?cmd=ka%200%2000%0D"
-# GS Portrait 2 / 
-wget -o /dev/null -O /dev/null "http://10.40.30.88/cgi-bin/cgi.pl?cmd=ka%200%2000%0D"
-# GS Portrait 3 / 
-wget -o /dev/null -O /dev/null "http://10.40.30.89/cgi-bin/cgi.pl?cmd=ka%200%2000%0D"
-# GS Portrait 4 / 
-wget -o /dev/null -O /dev/null "http://10.40.30.90/cgi-bin/cgi.pl?cmd=ka%200%2000%0D"
-# GS Portrait 5 / 
-wget -o /dev/null -O /dev/null "http://10.40.30.91/cgi-bin/cgi.pl?cmd=ka%200%2000%0D"
-# GS Portrait 6 / 
-wget -o /dev/null -O /dev/null "http://10.40.30.92/cgi-bin/cgi.pl?cmd=ka%200%2000%0D"
-
-
+# GS Cabinet HL /
+wget -o /dev/null -O /dev/null "http://10.40.30.85/cgi-bin/cgi.pl?cmd=ka%200%200%0D"
+# GS Cabinet HR /
+wget -o /dev/null -O /dev/null "http://10.40.30.86/cgi-bin/cgi.pl?cmd=ka%200%200%0D"
+# GS Portrait 1 /
+wget -o /dev/null -O /dev/null "http://10.40.30.87/cgi-bin/cgi.pl?cmd=ka%200%200%0D"
+# GS Portrait 2 /
+wget -o /dev/null -O /dev/null "http://10.40.30.88/cgi-bin/cgi.pl?cmd=ka%200%200%0D"
+# GS Portrait 3 /
+wget -o /dev/null -O /dev/null "http://10.40.30.89/cgi-bin/cgi.pl?cmd=ka%200%200%0D"
+# GS Portrait 4 /
+wget -o /dev/null -O /dev/null "http://10.40.30.90/cgi-bin/cgi.pl?cmd=ka%200%200%0D"
+# GS Portrait 5 /
+wget -o /dev/null -O /dev/null "http://10.40.30.91/cgi-bin/cgi.pl?cmd=ka%200%200%0D"
+# Pillar TVs sync
+# wget -o /dev/null -O /dev/null
+# GS Portrait 6 /
+wget -o /dev/null -O /dev/null "http://10.40.30.92/cgi-bin/cgi.pl?cmd=ka%200%200%0D"
+# Bookstore Menu TVs /
+wget -o /dev/null -O /dev/null "http://10.40.30.110/cgi-bin/raw.pl?cmd=ka%200%200%0D"
+# Pillar TVs off
+wget -o /dev/null -O /dev/null "http://10.40.30.97/cgi-bin/raw.pl?cmd=POWR%20%20%200%0D"
+# Jordan Creek Prayer TV off
+wget -o /dev/null -O /dev/null "http://10.40.30.111/cgi-bin/raw.pl?cmd=%02POF%03"
 
 
 # ***** ***** *****
