5b64e68768336f99cccffad3fb8acc3998d65316
willpile <willpile@users.noreply.github.com>
2018-01-30 09:45:31 -0600

    Update tv_auto-on.sh
    added pillar TVs, added volume 0 and HDMI input commands

--- Automation/tv_auto-on.sh
+++ Automation/tv_auto-on.sh
@@ -6,6 +6,7 @@
 
 # -----
 # Gathering Space
+# ON Command
 # -----
 
 # GS Cabinet HL / 
@@ -24,6 +25,64 @@ wget -o /dev/null -O /dev/null "http://10.40.30.90/cgi-bin/cgi.pl?cmd=ka%200%200
 wget -o /dev/null -O /dev/null "http://10.40.30.91/cgi-bin/cgi.pl?cmd=ka%200%2001%0D"
 # GS Portrait 6 / 
 wget -o /dev/null -O /dev/null "http://10.40.30.92/cgi-bin/cgi.pl?cmd=ka%200%2001%0D"
+# Bookstore Menu TVs / 
+wget -o /dev/null -O /dev/null "http://10.40.30.110/cgi-bin/raw.pl?cmd=ka%200%2001%0D"
+# Pillar TVs
+wget -o /dev/null -O /dev/null "http://10.40.30.97/cgi-bin/raw.pl?cmd=POWR%20%20%201%0D"
+sleep 7s
+
+
+# -----
+# Gathering Space
+# Input HDMI 1
+# -----
+
+# GS Cabinet HL / 
+wget -o /dev/null -O /dev/null "http://10.40.30.85/cgi-bin/cgi.pl?cmd=xb%200%2090%0D"
+# GS Cabinet HR / 
+wget -o /dev/null -O /dev/null "http://10.40.30.86/cgi-bin/cgi.pl?cmd=xb%200%2090%0D"
+# GS Portrait 1 / 
+wget -o /dev/null -O /dev/null "http://10.40.30.87/cgi-bin/cgi.pl?cmd=xb%200%2090%0D"
+# GS Portrait 2 / 
+wget -o /dev/null -O /dev/null "http://10.40.30.88/cgi-bin/cgi.pl?cmd=xb%200%2090%0D"
+# GS Portrait 3 / 
+wget -o /dev/null -O /dev/null "http://10.40.30.89/cgi-bin/cgi.pl?cmd=xb%200%2090%0D"
+# GS Portrait 4 / 
+wget -o /dev/null -O /dev/null "http://10.40.30.90/cgi-bin/cgi.pl?cmd=xb%200%2090%0D"
+# GS Portrait 5 / 
+wget -o /dev/null -O /dev/null "http://10.40.30.91/cgi-bin/cgi.pl?cmd=xb%200%2090%0D"
+# GS Portrait 6 / 
+wget -o /dev/null -O /dev/null "http://10.40.30.92/cgi-bin/cgi.pl?cmd=xb%200%2090%0D"
+# Bookstore Menu TVs HDMI2/ 
+wget -o /dev/null -O /dev/null "http://10.40.30.110/cgi-bin/raw.pl?cmd=xb%200%2091%0D"
+sleep 1s
+
+
+# -----
+# Gathering Space
+# Volume = 0
+# -----
+
+# GS Cabinet HL / 
+wget -o /dev/null -O /dev/null "http://10.40.30.85/cgi-bin/cgi.pl?cmd=kf%200%2000%0D"
+# GS Cabinet HR / 
+wget -o /dev/null -O /dev/null "http://10.40.30.86/cgi-bin/cgi.pl?cmd=kf%200%2000%0D"
+# GS Portrait 1 / 
+wget -o /dev/null -O /dev/null "http://10.40.30.87/cgi-bin/cgi.pl?cmd=kf%200%2000%0D"
+# GS Portrait 2 / 
+wget -o /dev/null -O /dev/null "http://10.40.30.88/cgi-bin/cgi.pl?cmd=kf%200%2000%0D"
+# GS Portrait 3 / 
+wget -o /dev/null -O /dev/null "http://10.40.30.89/cgi-bin/cgi.pl?cmd=kf%200%2000%0D"
+# GS Portrait 4 / 
+wget -o /dev/null -O /dev/null "http://10.40.30.90/cgi-bin/cgi.pl?cmd=kf%200%2000%0D"
+# GS Portrait 5 / 
+wget -o /dev/null -O /dev/null "http://10.40.30.91/cgi-bin/cgi.pl?cmd=kf%200%2000%0D"
+# GS Portrait 6 / 
+wget -o /dev/null -O /dev/null "http://10.40.30.92/cgi-bin/cgi.pl?cmd=kf%200%2000%0D"
+# Bookstore Menu TVs / 
+wget -o /dev/null -O /dev/null "http://10.40.30.110/cgi-bin/raw.pl?cmd=kf%200%2000%0D"
+
+
 
 
 
