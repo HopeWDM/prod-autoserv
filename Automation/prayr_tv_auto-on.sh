44598852b2b99003f63eb39ebaabfd46277117b1
willpile <willpile@users.noreply.github.com>
2018-01-30 09:40:46 -0600

    Create prayr_tv_auto-on.sh

---
+++ Automation/prayr_tv_auto-on.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# location:
+# /opt/scripts/projector-automation/jc_tv_auto-on.sh
+
+
+# -----
+# Prayer Room TVs
+# ON Command
+# -----
+
+# Jordan Creek Prayer Room
+wget -o /dev/null -O /dev/null "http://10.40.30.111/cgi-bin/raw.pl?cmd=%02PON%03"
+
+
+# EOF
