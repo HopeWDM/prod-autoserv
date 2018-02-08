675c592f954a6771cd4a1a284b80f88681921fb1
willpile <willpile@users.noreply.github.com>
2018-02-08 12:48:35 -0600

    Update tv_auto-off.sh
    Added JC Prayer, Ashworth Prayer, Nursing Mothers, Chapel Entry, Youth Entry, Circle Drive Entry TV's

--- Automation/tv_auto-off.sh
+++ Automation/tv_auto-off.sh
@@ -28,10 +28,20 @@ wget -o /dev/null -O /dev/null "http://10.40.30.91/cgi-bin/cgi.pl?cmd=ka%200%200
 wget -o /dev/null -O /dev/null "http://10.40.30.92/cgi-bin/cgi.pl?cmd=ka%200%200%0D"
 # Bookstore Menu TVs /
 wget -o /dev/null -O /dev/null "http://10.40.30.110/cgi-bin/raw.pl?cmd=ka%200%200%0D"
-# Pillar TVs off
+# Pillar TVs off /
 wget -o /dev/null -O /dev/null "http://10.40.30.97/cgi-bin/raw.pl?cmd=POWR%20%20%200%0D"
-# Jordan Creek Prayer TV off
+# Jordan Creek Prayer TV off /
 wget -o /dev/null -O /dev/null "http://10.40.30.111/cgi-bin/raw.pl?cmd=%02POF%03"
+# Ashworth Prayer TV off /
+wget -o /dev/null -O /dev/null "http://10.40.30.112/cgi-bin/raw.pl?cmd=%02POF%03"
+# Nursing Mothers TV /
+wget -o /dev/null -O /dev/null "http://10.40.30.113/cgi-bin/raw.pl?cmd=ka%200%200%0D"
+# Chapel Entry TV /
+wget -o /dev/null -O /dev/null "http://10.40.30.115/cgi-bin/raw.pl?cmd=POWR%20%20%200%0D"
+# Youth Entry TV /
+wget -o /dev/null -O /dev/null "http://10.40.30.116/cgi-bin/raw.pl?cmd=POWR%20%20%200%0D"
+# Circle Dr Entry TV /
+wget -o /dev/null -O /dev/null "http://10.40.30.117/cgi-bin/raw.pl?cmd=POWR%20%20%200%0D"
 
 
 # ***** ***** *****
