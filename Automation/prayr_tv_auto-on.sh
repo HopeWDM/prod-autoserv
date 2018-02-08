afd0d070005ff7aa34dd0760f065bf7ce13d9b58
willpile <willpile@users.noreply.github.com>
2018-02-08 12:44:03 -0600

    Update prayr_tv_auto-on.sh
    added Ashworth Prayer and Nursing Mother's lounge TV's

--- Automation/prayr_tv_auto-on.sh
+++ Automation/prayr_tv_auto-on.sh
@@ -11,6 +11,9 @@
 
 # Jordan Creek Prayer Room
 wget -o /dev/null -O /dev/null "http://10.40.30.111/cgi-bin/raw.pl?cmd=%02PON%03"
-
+# Ashworth Prayer Room
+wget -o /dev/null -O /dev/null "http://10.40.30.112/cgi-bin/raw.pl?cmd=%02PON%03"
+# Nursing Mothers TV /
+wget -o /dev/null -O /dev/null "http://10.40.30.113/cgi-bin/raw.pl?cmd=ka%200%201%0D"
 
 # EOF
