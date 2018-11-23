9f2a543383e8745a3d224891a96176d7c1f03c75
willpile <willpile@users.noreply.github.com>
2018-11-23 13:32:37 -0600

    Update Hyperdeck status
    modified command and variables to operate the relay python scripts on the rpi.

--- Automation/BlackMagic-HyperDeck/hyperdeck-status.pl
+++ Automation/BlackMagic-HyperDeck/hyperdeck-status.pl
@@ -8,11 +8,19 @@ our $ip_addr_1 = '10.40.31.120';
 our $ip_addr_2 = '10.40.31.121';
 our $nl = "\n";
 
+our $relay_on_1 = 'relay_1_on.py';
+our $relay_on_2 = 'relay_2_on.py';
+
+our $relay_off_1 = 'relay_1_off.py';
+our $relay_off_2 = 'relay_2_off.py';
+
 sub statuser {
         my $ip_addr = shift;
+	my $relay_on = shift;
+	my $relay_off = shift;
 	#my $pick_slot = shift;
         #print "This run:\n";
-        #print "IP Address: $ip_addr\n";
+        print "IP Address: $ip_addr\n";
         #print "$pick_slot\n";
         my @output;
         my $telnet = new Net::Telnet (Timeout=>10, Errmode=>'return', Port=>9993);
@@ -25,16 +33,19 @@ sub statuser {
 	print $value.$nl; # show us what we care about
 	if ( $value eq "record" ) {
 		print "we are recording".$nl;
-		system('echo "this is a test system command" | wall');
+		print "relay on: $relay_on:".$nl;
+		system("python /home/production/python/$relay_on");
 	} else {
 		print "we areN'T recording".$nl;
-	}
+		print "relay off: $relay_off:".$nl;
+		system("python /home/production/python/$relay_off");
+		}
 }
 
 sub run_statuses {
-        statuser ($ip_addr_1);
+        statuser ($ip_addr_1, $relay_on_1, $relay_off_1);
 	sleep 2;
-        statuser ($ip_addr_2);
+        statuser ($ip_addr_2, $relay_on_2, $relay_off_2);
 }
 
 run_statuses;
